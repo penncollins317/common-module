c9187a49b24b95117b3ebbd1abb5654d30e088b9
Peng <qianmeng6879@163.com>
2025-04-28 08:08:39 +0800

    [ADD]完善

---
+++ multi-db-samples/src/main/java/top/mxzero/multi/db/MultiDBConfig.java
@@ -0,0 +1,13 @@
+package top.mxzero.multi.db;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Peng
+ * @since 2025/4/25
+ */
+@SpringBootApplication
+@MapperScan("top.mxzero.multi.db.mapper")
+public class MultiDBConfig {
+}
