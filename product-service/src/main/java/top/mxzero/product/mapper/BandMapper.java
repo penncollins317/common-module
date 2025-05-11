2205d3a23c124dbb10d1c07c7fcd589570d1baf3
Peng <qianmeng6879@163.com>
2025-05-11 20:57:17 +0800

    [ADD]品牌相关接口

---
+++ product-service/src/main/java/top/mxzero/product/mapper/BandMapper.java
@@ -0,0 +1,13 @@
+package top.mxzero.product.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import top.mxzero.product.entity.Band;
+
+/**
+ * @author Peng
+ * @since 2025/5/11
+ */
+@Mapper
+public interface BandMapper extends BaseMapper<Band> {
+}
--- product-service/src/main/resources/product_service_postgresql.sql
+++ product-service/src/main/resources/product_service_postgresql.sql
@@ -10,3 +10,11 @@ CREATE TABLE product_category(
 
 CREATE UNIQUE INDEX uq_product_category_name_parent ON product_category(parent_id, name);
 CREATE INDEX idx_product_category_path ON product_category(path);
+
+CREATE TABLE band(
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    image_url VARCHAR(255) NULL
+);
+
+CREATE UNIQUE INDEX band_name_uk ON band(name);
