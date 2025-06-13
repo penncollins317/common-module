e692b08efe03fdf18eaa97f2852c389c4ed7da90
Peng <qianmeng6879@163.com>
2025-06-13 23:53:25 +0800

    [FIX]完善jwt参数

--- security-jwt/src/main/java/top/mxzero/security/jwt/JwtProps.java
+++ security-jwt/src/main/java/top/mxzero/security/jwt/JwtProps.java
@@ -3,6 +3,8 @@ package top.mxzero.security.jwt;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.UUID;
+
 /**
  * @author Peng
  * @since 2025/2/13
@@ -10,8 +12,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Data
 @ConfigurationProperties("mxzero.jwt")
 public class JwtProps {
-    private String issuer;
-    private String secret;
+    private String issuer = "default";
+    private String secret = UUID.randomUUID().toString();
     private long expire = 7200L;
     private int refreshRate = 3;
 }
